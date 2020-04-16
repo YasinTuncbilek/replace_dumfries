bda71d6306e7533f4cda5036af6ef82fe30b2992
Yasin Tuncbilek <y_tuncbilek@hotmail.com>
2020-04-16 23:55:11 +0200

    Update.

--- Script PCA & Cluster Analysis.R
+++ Script PCA & Cluster Analysis.R
@@ -49,6 +49,9 @@ data2$'Tackles won per 90' <- (data2$`Tackles per 90` * data2$`Tackle succ. %`)/
 # Run PCA 
 pca <- prcomp(na.omit(data2[, 16:29]), scale = TRUE)
 
+# Plot PCA
+plot(pca, type = "l")
+
 # Get eigenvalues of principal components (> 1)
 eig <- (pca$sdev)^2
 
@@ -58,27 +61,27 @@ var <- eig*100/sum(eig)
 # Get cumulative variances
 cumvar <- cumsum(var)
 
-### End ###
-
 # Combine to dataframe
 eig_var_cum <- data.frame(eig = eig, variance = var, cumvariance = cumvar)
 
-### Principal Component Analysis with principal function ###
+# There are 6 factors with an eigenvalue of > 1
+
+### Another method: Principal Component Analysis with principal function of psych package ###
 # Load psych package
 library(psych)
 
 # Run PCA
-principal <- principal(data2[, 16:29], nfactors = 6, rotate = "varimax")
-
-### End ###
-
-### Heatmap of factor loadings ###
-# Load relevant packages
-library(gplots)
-library(RColorBrewer)
+principal <- principal(na.omit(data2[, 16:29]), nfactors = 6, rotate = "varimax")
 
 # Sort loadings
 principal$loadings <- fa.sort(principal$loadings)
+
+### End ###
+
+### Heatmap of factor loadings ### --> Deze verbeteren
+# Load relevant packages
+library(gplots)
+library(RColorBrewer)
 
 # Create heatmap 
 heatmap.2(principal$loadings, trace = "none", dend = "none", main = "DR/WBR playing style profiles", 
@@ -91,7 +94,46 @@ heatmap.2(principal$loadings, trace = "none", dend = "none", main = "DR/WBR play
 ### End ###
 
 ### Cluster Analysis ###
-d_data <- dist(na.omit(data2[, 16:29]), method = "euclidean")
-hc_data <- hclust(d_data, method = "ward.D2") 
-plot(hc_data, hang = -1, cex = 1)
+# Select players who played > 650 minutes
+data3 <- data2 %>%
+  filter(`Minutes played` > 650)
+
+# Scale data
+data_scaled <- scale(data3[, 16:29])
+
+# Run Cluster Analysis
+d_data <- dist(data_scaled, method = "euclidean")
+hc_data <- hclust(d_data, method = "complete") 
+
+# Use cutreeDynamic to determine numbers of clusters
+library(dynamicTreeCut)
+clusters <- cutreeDynamic(hc_data, cutHeight = NULL, minClusterSize = 3, method = "hybrid", distM = as.matrix(d_data))
+clusters
+
+### Dendrogram ###
+# Load relevant packages
+library(dplyr)
+library(dendextend)
+library(circlize)
+
+# Save as dendrogram
+dend <- hc_data %>% as.dendrogram 
+
+# Use player names as labels
+labels(dend) <- data3$Player[hc_data$order]
+
+# Create custom colors for clusters
+cols_branches <- c("blue", "red", "yellow", "green", "pink", "purple",
+                   "brown", "gray", "black", "azure", "aquamarine",
+                   "deepskyblue", "darksalmon", "khaki", "lightgreen", "orange",
+                   "peru", "seagreen", "tan", "yellowgreen", "violet")
+
+# Provide colors to the branches and labels
+dend <- dend %>% 
+  color_branches(k = 21, col = cols_branches) %>%
+  color_labels(col = "black")
+
+par(cex = 0.55, mar = c(5, 8, 4, 1)) 
+circlize_dendrogram(dend, labels = TRUE) 
+title(main = "Playing style partners of players on RB/RWB-position")
 
